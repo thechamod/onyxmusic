6d699dd327dc795cec7720712849e89ca7971aa0
thechamod <gimhanwijekoon5@gmail.com>
2025-05-05 03:38:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './App.tsx';
 import { FirebaseProvider } from './context/FirebaseContext';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// This is the standard way to render a React app in React 18
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <BrowserRouter>
       <FirebaseProvider>
         <App />
       </FirebaseProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>,
 );
