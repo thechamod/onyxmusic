e7bb2f82cc100abc833a98c09d3bad8785bc21b9
thechamod <gimhanwijekoon5@gmail.com>
2025-05-10 10:17:58 +0530

    Update ArtistsPreview.tsx

--- src/components/home/ArtistsPreview.tsx
+++ src/components/home/ArtistsPreview.tsx
@@ -40,17 +40,17 @@ const ArtistsPreview: React.FC = () => {
   const artists = [
     {
       name: 'Y30',
-      image: 'https://z-p3-scontent.fcmb7-1.fna.fbcdn.net/v/t39.30808-6/481479195_1846203492834089_8896037436071034314_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeG9se2S4uUrIKc6HXGmKbZH9I5UT-9nTV_0jlRP72dNXxjC28SVEKyt9vOZ4we0zBL_1NqMtP95C-CG2f2LqKBJ&_nc_ohc=499dZoEbFyYQ7kNvwEr9pE2&_nc_oc=AdnpLqT_-NwwBKGxJO_y1DOWFNQe4VTeUI1JnqHIbtieRmVM9jR75MlkMM2rZGDqsw0&_nc_zt=23&_nc_ht=z-p3-scontent.fcmb7-1.fna&_nc_gid=H7ZkQqM8OwwSKWtiama6IA&oh=00_AfGcyR_nJUNqkIMA5UmPQ7w3ui5S453iBmtOuhnSq4C6_A&oe=681BD102',
+      image: 'https://files.catbox.moe/my8lio.jpg',
       role: 'Rapper / Songwriter'
     },
     {
       name: 'W',
-      image: 'https://z-p3-scontent.fcmb7-1.fna.fbcdn.net/v/t39.30808-6/472345858_1312545273091867_1276585712862569140_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHH1St2HWdJ1ycZshwEs8qeTeCvnX8POL5N4K-dfw84vm1Euf6EJDdWakRjZgUSPhvx8K9dvbxgqzfYLkc6I6lr&_nc_ohc=3XMsGl_d390Q7kNvwELHKp9&_nc_oc=AdkG2TzzmHJVnDZkE0siOOgHdEW2UyI5u_RUAuI3hvpvXmpY4zWwh4_r28bTho79kxI&_nc_zt=23&_nc_ht=z-p3-scontent.fcmb7-1.fna&_nc_gid=2qGLrYOLxZiu_efh4y4AKg&oh=00_AfHlqd2fzVbR9LTM7QtFtfmga7Pc0LkVxa93PRhinv4XoA&oe=681BDA23',
+      image: 'https://files.catbox.moe/93icrh.jpg',
       role: 'Rapper / Songwriter'
     },
     {
       name: 'RIX',
-      image: 'https://z-p3-scontent.fcmb7-1.fna.fbcdn.net/v/t39.30808-6/485040617_688294940374187_6007777225130452546_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHRaMrqtxX_GW3SDIFMNHGPiCCWyt45Cy6IIJbK3jkLLhl1rJn7in3oWNT4yyMMJtXxOfBiVnC0WipO34RLpBuW&_nc_ohc=73WHcWlP3woQ7kNvwGDHOnE&_nc_oc=AdnDyk5Kr112UfVCm5dgjzNE_8aks9_FxeR2IAfbN6OyBeYHQM_Lomb4eUmnOfbw78M&_nc_zt=23&_nc_ht=z-p3-scontent.fcmb7-1.fna&_nc_gid=VHucCeorgIdEMWs2cqOohA&oh=00_AfFe7uV0QgFt6_R_Vsm5-SsrSQ7Dxpl9C9KKttVbfT8ydw&oe=681BD295',
+      image: 'https://files.catbox.moe/vqvq5q.jpg',
       role: 'Rapper / Songwriter'
     } 
   ];
